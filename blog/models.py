b392473565a54cc16c24e7cec252bdb1d3ed2429
jessicarydberg <jessica.rydberg@hotmail.se>
2022-06-23 13:47:50 +0000

    Finish comment function

--- blog/models.py
+++ blog/models.py
@@ -19,13 +19,13 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created_on']
-    
+
     def __str__(self):
         return self.title
 
     def number_of_likes(self):
         return self.likes.count()
-    
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
@@ -36,6 +36,6 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['created_on']
-    
+
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
